f9eecca3a76a9fdb1cc89768dfe89787ead4a19a
falconzs <falconfiftyfive@yahoo.co.uk>
2016-03-08 05:47:15 +0000

    Intermediate commit of some copy/pasted code.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ var DefineClass = require('define-class'),
     TimerPlugin = DefineClass(PluginAbstract, {
 
         init: function() {
+            this.startTime = 0;
+            this.timeAdded = 0;
+
             this.commands = {
                 starttimer: {
                     access: ['host', 'mod'],
@@ -29,6 +32,53 @@ var DefineClass = require('define-class'),
         },
 
         startTimer: function(fromUser, noOfMinutes) {
+            // There is no timer already running
+            if (startTime == 0 && timeAdded == 0) {
+                // Check if a valid option was sent with /starttimer
+                if (t >= 0 && t.toString().indexOf(".") == -1) {
+                    timerDuration = t;
+                    // Notice of timer start
+                    if (mod != null) {
+                        this.api.sendNotice(fromUser.name + ' has set a timer for ' + timerDuration + ' minutes!', '', purple);
+                    }
+                    // Local variable to convert noticeTime (minutes) to milliseconds
+                    var millis = timerDuration * 60000;
+                    var fiveMinutes = millis - 300000;
+                    var oneMinute = millis - 60000;
+
+                    // Actual timer
+                    this.api.setTimeout(this.calculateTime.bind(this), millis);
+                    // Five minutes remaining announcement
+                    if (fiveMinutes > 0) {
+                        cb.setTimeout(fiveMinuteWarning, fiveMinutes);
+                    }
+                    // One minute remaining announcement
+                    this.api.setTimeout(oneMinuteWarning, oneMinute);
+                    // Set the start time
+                    startTime = new Date();
+                } else if (t != null) {
+                    this.api.sendNotice(t + ' is not a valid option for /starttimer.\nType /ubhelp starttimer to see how to use /starttimer.', mod, "#fee");
+                } else if (t == null) {
+                    this.api.sendNotice('You did not enter a valid option for /starttimer.\nType /ubhelp starttimer to see how to use /starttimer.', mod, "#fee");
+                }
+            } else if (startTime != 0 && timeAdded != 0 && mod == null) {
+                // There is a timer running and time has been added
+                this.timeAdded = 0;
+                this.timerDuration = t;
+                var millis = timerDuration * 60000;
+                var fiveMinutes = millis - 300000;
+                var oneMinute = millis - 60000;
+
+                cb.setTimeout(timer, millis);
+
+                if (fiveMinutes > 0) {
+                    this.api.setTimeout(fiveMinuteWarning, fiveMinutes);
+                }
+                this.api.setTimeout(oneMinuteWarning, oneMinute);
+            } else if (startTime != 0 && timeAdded == 0 || startTime != 0 && timeAdded != 0 && mod != null) {
+                // There is a timer running and someone tried to start a new timer
+                this.api.sendNotice('There is a timer running already.', fromUser.name, "#fee");
+            }
         },
 
         stopTimer: function() {
@@ -38,6 +88,10 @@ var DefineClass = require('define-class'),
         },
 
         timeLeft: function(fromUser) {
+        },
+
+        calculateTime: function() {
+
         }
     });
 
